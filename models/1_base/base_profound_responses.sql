c6f140d87da07ce51354357e711d402cb08921ad
Kyle Cielencki <kylecielencki@mac.com>
2026-05-21 12:11:51 -0500

    Update base_profound_responses.sql

--- models/1_base/base_profound_responses.sql
+++ models/1_base/base_profound_responses.sql
@@ -12,7 +12,7 @@ SELECT DISTINCT CAST(created_at AS date) AS date
     , LOWER(region) AS region
     , LOWER(prompt) AS prompt
     , LOWER(REGEXP_REPLACE(response, '[\r\n]+', ' ')) AS response
-    , LOWER(nike_present) AS nike_present
-    , CAST(nike_rank_among_brands AS {{ type_float() }}) AS  nike_response_rank
+    , LOWER(nike_present) AS brand_present
+    , CAST(nike_rank_among_brands AS {{ type_float() }}) AS  brand_response_rank
     , LOWER(brands_in_order) AS response_brands_in_order
 FROM RAW_DATA
